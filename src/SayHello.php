57c2d89756dc3ec5324189af880bc30b534abc1a
knowcup <knowcup@163.com>
2018-01-16 14:41:53 +0800

    first submit

---
+++ src/SayHello.php
@@ -0,0 +1,15 @@
+<?php
+namespace Hello;
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/1/15
+ * Time: 20:41
+ */
+
+class SayHello
+{
+    public static function say(){
+        echo "Hello World";
+    }
+}
---
+++ test/SayHelloTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: keler <knowcup@163.com>
+ * Date: 2018/1/15
+ * Time: 20:49
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Hello\SayHello;
+
+SayHello::say();
